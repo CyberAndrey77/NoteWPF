846dabb6867d1981da03c1613e7c60d5bd5213ff
CyberAndrey77 <lapardin.andrey@mail.ru>
2022-07-28 21:28:04 +0700

    Добавлен сервис JsonSerializerSevice

---
+++ src/NoteWpf/Services/JsonSerializerService.cs
@@ -0,0 +1,18 @@
+﻿using NoteWpf.Services.Interface;
+using System.Text.Json;
+
+namespace NoteWpf.Services
+{
+    public class JsonSerializerService : IJsonSerializerService
+    {
+        public T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public string Serialize<T>(T obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+    }
+}
